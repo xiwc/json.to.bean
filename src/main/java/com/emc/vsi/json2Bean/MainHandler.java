87625365a52bc3c6248ad39409bad40e6df4b5c7
weichx <Xi.Weicheng@emc.com>
2014-12-03 17:54:03 +0800

    use freemark tmpl to create java file.

--- src/main/java/com/emc/vsi/json2Bean/MainHandler.java
+++ src/main/java/com/emc/vsi/json2Bean/MainHandler.java
@@ -2,6 +2,7 @@ package com.emc.vsi.json2Bean;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,11 +14,12 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
 public class MainHandler {
 
 	public static final String FILE_PATH = "C:\\json-2-bean\\";
-	private static final String PKG_PATH = "package com.emc.vsi.providers.data;";
-	private static final String AUTHOR = "weichx";
 	private static ILogger logger;
 	private static Map<String, Object> objMap = new HashMap<String, Object>();
 
@@ -25,59 +27,26 @@ public class MainHandler {
 
 		MainHandler.logger = logger;
 
+		logger.info("delete then force mkdir [%s].", FILE_PATH);
+
 		FileUtils.deleteDirectory(new File(FILE_PATH));
-		logger.info("force mkdir [%s].", FILE_PATH);
 		FileUtils.forceMkdir(new File(FILE_PATH));
 
 		objMap.clear();
 
-		logger.info("start parse json object");
+		logger.info(">>>start parse json object");
+
 		parseJSONObject(null, JSON.parseObject(json));
-		logger.info("end parse json object");
-	}
 
-	private static void writeln(File file, String line) throws IOException {
-
-		if (file != null) {
-			FileUtils.write(file, line, true);
-			FileUtils.write(file, "\r\n", true);
-		}
+		logger.info("<<<end parse json object");
 	}
 
 	private static void parseJSONObject(String name, JSONObject jsonObject) throws IOException {
 
-		File file = null;
+		BeanInfo beanInfo = new BeanInfo();
 
 		if (!StringUtils.isEmpty(name)) {
-
-			String clsName = StringUtils.capitalize(name);
-
-			file = new File(String.format("%s%s.java", FILE_PATH, clsName));
-
-			if (file.exists()) {
-
-				clsName = String.format("%s%s", StringUtils.capitalize(name), String.valueOf(new Date().getTime()));
-
-				file = new File(String.format("%s%s.java", FILE_PATH, clsName));
-
-				FileUtils.write(file, "");
-
-				logger.info("file [%s] exists, will create a new file[%s]",
-						String.format("%s%s.java", FILE_PATH, StringUtils.capitalize(name)), file.getParent());
-			} else {
-				logger.info("file [%s] not exists, will create new.",
-						String.format("%s%s.java", FILE_PATH, StringUtils.capitalize(name)));
-			}
-
-			writeln(file, PKG_PATH);
-			writeln(file, "");
-			writeln(file, "/**");
-			writeln(file, "*");
-			writeln(file, "* @author " + AUTHOR);
-			writeln(file, "*");
-			writeln(file, "*/");
-			writeln(file, "public class " + clsName + " {");
-			writeln(file, "");
+			beanInfo.setClsName(StringUtils.capitalize(name));
 		}
 
 		Map<String, Object> map = new HashMap<String, Object>();
@@ -87,29 +56,29 @@ public class MainHandler {
 			Object object = jsonObject.get(key);
 
 			if (object instanceof String || object == null) {
-				System.out.println(String.format("private String %s;", key));
 				logger.info("private String %s;", key);
-				writeln(file, String.format("private String %s;", key));
+
+				beanInfo.getFields().add(new Field(key, String.class.getSimpleName()));
 			} else if (object instanceof Boolean) {
-				System.out.println(String.format("private Boolean %s;", key));
 				logger.info("private Boolean %s;", key);
-				writeln(file, String.format("private Boolean %s;", key));
+
+				beanInfo.getFields().add(new Field(key, Boolean.class.getSimpleName()));
 			} else if (object instanceof Integer) {
-				System.out.println(String.format("private Integer %s;", key));
 				logger.info("private Integer %s;", key);
-				writeln(file, String.format("private Integer %s;", key));
+
+				beanInfo.getFields().add(new Field(key, Integer.class.getSimpleName()));
 			} else if (object instanceof Long) {
-				System.out.println(String.format("private Long %s;", key));
 				logger.info("private Long %s;", key);
-				writeln(file, String.format("private Long %s;", key));
+
+				beanInfo.getFields().add(new Field(key, Long.class.getSimpleName()));
 			} else if (object instanceof Short) {
-				System.out.println(String.format("private Short %s;", key));
 				logger.info("private Short %s;", key);
-				writeln(file, String.format("private Short %s;", key));
+
+				beanInfo.getFields().add(new Field(key, Short.class.getSimpleName()));
 			} else if (object instanceof Character) {
-				System.out.println(String.format("private Character %s;", key));
 				logger.info("private Character %s;", key);
-				writeln(file, String.format("private Character %s;", key));
+
+				beanInfo.getFields().add(new Field(key, Character.class.getSimpleName()));
 			} else if (object instanceof JSONObject) {
 
 				String newK = key;
@@ -125,9 +94,10 @@ public class MainHandler {
 
 				objMap.put(newK, null);
 
-				System.out.println(String.format("private %s %s;", StringUtils.capitalize(newK), key));
 				logger.info("private %s %s;", StringUtils.capitalize(newK), newK);
-				writeln(file, String.format("private %s %s;", StringUtils.capitalize(newK), key));
+
+				beanInfo.getFields().add(new Field(key, StringUtils.capitalize(newK)));
+
 				map.put(newK, object);
 			} else if (object instanceof JSONArray) {
 
@@ -150,20 +120,18 @@ public class MainHandler {
 
 				objMap.put(clsName, null);
 
-				System.out.println(String.format("private List<%s> %s;", StringUtils.capitalize(clsName), key));
 				logger.info("private List<%s> %s;", StringUtils.capitalize(clsName), key);
-				writeln(file, String.format("private List<%s> %s;", StringUtils.capitalize(clsName), key));
+
+				beanInfo.getFields().add(new Field(key, StringUtils.capitalize(clsName)));
+
 				map.put(clsName, ((JSONArray) object).getJSONObject(0));
 			} else {
-				System.err.println(String.format("Key: %s, Value: %s, Unrecognized Type: %s", key,
-						String.valueOf(object), object.getClass().getName()));
 				logger.error("Key: %s, Value: %s, Unrecognized Type: %s", key, String.valueOf(object), object
 						.getClass().getName());
 			}
 		}
 
-		writeln(file, "");
-		writeln(file, "}");
+		createJavaFile(beanInfo);
 
 		for (String key : map.keySet()) {
 
@@ -171,14 +139,40 @@ public class MainHandler {
 
 			if (object instanceof JSONObject) {
 
-				System.out.println();
-				System.out.println(String.format("JSONObject: [%s] %s", StringUtils.capitalize(key), key));
 				logger.info("JSONObject: [%s] %s", StringUtils.capitalize(key), key);
 
 				parseJSONObject(key, (JSONObject) object);
-
-				System.out.println();
 			}
+		}
+	}
+
+	private static void createJavaFile(BeanInfo beanInfo) {
+
+		if (beanInfo == null || beanInfo.getClsName() == null) {
+			return;
+		}
+
+		try {
+			Configuration configuration = new Configuration(Configuration.VERSION_2_3_21);
+			configuration.setClassicCompatible(true);
+			configuration.setClassForTemplateLoading(MainHandler.class, "");
+			configuration.setDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+			configuration.setNumberFormat("");
+			configuration.setDefaultEncoding("utf-8");
+			Template template = configuration.getTemplate("Bean.ftl");
+
+			StringWriter sw = new StringWriter();
+
+			Map<String, Object> dataModelMap = new HashMap<String, Object>();
+			dataModelMap.put("m", beanInfo);
+
+			template.process(dataModelMap, sw);
+
+			File file = new File(FILE_PATH + beanInfo.getClsName() + ".java");
+			FileUtils.write(file, sw.toString(), "utf-8");
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
 		}
 	}
 }
