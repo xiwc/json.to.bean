8804685819b8b8d6c4ac2b118378e26d73e2d7b9
weichx <Xi.Weicheng@emc.com>
2014-12-08 16:45:34 +0800

    new ui design...

---
+++ src/main/java/com/emc/vsi/json2Bean/MainPanel.java
@@ -0,0 +1,11 @@
+package com.emc.vsi.json2Bean;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+
+public class MainPanel extends JPanel {
+	public MainPanel() {
+		setLayout(new BorderLayout(0, 0));
+	}
+}
